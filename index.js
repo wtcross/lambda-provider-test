3451b725a62d2810325e3e6c1bbfba0b559a248c
Tyler Cross <tcross@bandwidth.com>
2014-12-02 16:55:24 -0500

    file

--- index.js
+++ index.js
@@ -1,4 +1,35 @@
 "use strict";
-var handler = require("./lib/handler");
+var aws  = require("aws-sdk");
+var util = require("util");
+var Q    = require("q");
 
-exports.handler = handler;
+console.log("Loading event");
+
+var s3 = new aws.S3({
+	apiVersion : "2006-03-01"
+});
+
+exports.handler = function (event, context) {
+	console.log("Received event:");
+	console.log(JSON.stringify(event, null, "  "));
+
+	// Get the object from the event and show its content type
+	var bucket = event.Records[0].s3.bucket.name;
+	var key = event.Records[0].s3.object.key;
+
+	Q.ninvoke(s3, "getObject", { Bucket : bucket, Key : key })
+	.catch(function () {
+		console.log(util.format(
+			[
+				"Error getting object %s from bucket %s.",
+				"Make sure they exist and your bucket is in the same region as this function."
+			].join(" "),
+			key,
+			bucket
+		));
+	})
+	.then(function (data) {
+		console.log("CONTENT TYPE:", data.ContentType);
+	})
+	.nodeify(context.done);
+};
